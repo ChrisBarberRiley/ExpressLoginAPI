02e03d7f197b021d7977f94423e82b429954bd6c
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-11-02 23:53:23 +0000

    POST / GET routes setup

--- controllers/pages.js
+++ controllers/pages.js
@@ -3,32 +3,64 @@ const Pages = require("../models/Pages");
 // @desc        Get all pages
 // @route       GET /api/v1/pages
 // @access      Public
-exports.getPages = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: "Show all pages"
-    });
+exports.getPages = async (req, res, next) => {
+    try {
+        const data = await Pages.find();
+
+        res.status(200).json({
+            success: true,
+            msg: "Show all pages",
+            data
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            msg: err
+        });
+    }
 };
 
 // @desc        Get specific page
 // @route       GET /api/v1/pages/:id
 // @access      Public
-exports.getSinglePage = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show specific page ${req.params.id}`
-    });
+exports.getSinglePage = async (req, res, next) => {
+    try {
+        const data = await Pages.findById(req.params.id);
+
+        if (!data) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            msg: `Show specific page ${req.params.id}`,
+            data
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            msg: `Failed to get page ${req.params.id} - ${err}`
+        });
+    }
 };
 
 // @desc        Create specific page
 // @route       POST /api/v1/pages/
 // @access      Public
-exports.createSinglePage = (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({
-        success: true,
-        msg: `Create new page`
-    });
+exports.createSinglePage = async (req, res, next) => {
+    try {
+        const page = await Pages.create(req.body);
+        res.status(201).json({
+            success: true,
+            msg: `Create new page`,
+            data: page
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            msg: `Failed to submit`
+        });
+    }
 };
 
 // @desc        Delete specific page
