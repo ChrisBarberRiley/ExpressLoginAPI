26018212d2aa7f45cd74c3c0b148b0e1830f4488
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-11-02 12:47:19 +0000

    Connected to MongoDB Atlas,  setup nicer console logging

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
-
-// Require routes
-const pages = require("./routes/pages");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load application config vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+// Require routes
+const pages = require("./routes/pages");
+
 const app = express();
 
-app.use(logger);
+// Dev logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/pages", pages);
 
 // Declare port and run server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+const server = app.listen(PORT, () =>
+    console.log(`App listening on port ${PORT}`.yellow.bold)
+);
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    // Close server and exit process
+    server.close(() => process.exit(1));
+});
