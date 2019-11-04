325cac0fdbb0a622ef57f3151495dd8e4743b3c0
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-11-04 22:05:26 +0000

    Login user and return JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,3 +25,42 @@ exports.register = async (req, res, next) => {
         next(err);
     }
 };
+
+// @desc        Login user
+// @route       POST /api/v1/auth/login
+// @access      Public
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        // Make sure email and password exists
+        if (!email || !password) {
+            return next(
+                new ErrorResponse("Please provide an email and password", 400)
+            );
+        }
+
+        // Check user exists
+        const user = await User.findOne({ email }).select("+password");
+
+        if (!email) {
+            return next(new ErrorResponse("Invalid credentials", 401));
+        }
+
+        // Check if password matches stored password
+        const isMatch = await user.matchPassword(password);
+
+        if (!isMatch) {
+            return next(new ErrorResponse("Invalid credentials", 401));
+        }
+
+        const token = user.getSignedJwtToken();
+
+        res.status(200).json({
+            success: true,
+            token
+        });
+    } catch (err) {
+        next(err);
+    }
+};
--- models/User.js
+++ models/User.js
@@ -49,4 +49,9 @@ UserSchema.methods.getSignedJwtToken = function() {
     });
 };
 
+// Match user enter to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("user", UserSchema);
