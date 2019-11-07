c59a1251951d953438e5be83bf0772dd2d2aea83
Chris Barber-Riley <chris.barberriley@gmail.com>
2019-11-07 19:53:28 +0000

    Improved error messages

--- middleware/error.js
+++ middleware/error.js
@@ -7,9 +7,22 @@ const errorHandler = (err, req, res, next) => {
     // Console log for devs
     console.log(err.stack.red);
 
+    // Mongoose bad object
     if (err.name === "CastError") {
         const message = `Resource (${err.value}) not found`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code == 11000) {
+        const message = "Duplicate field value entered";
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.code == "ValidationError") {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
